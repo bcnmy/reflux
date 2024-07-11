8ebf6897e90f7fd53a7981eaf0eb85e3b480b33f
Ankur Dubey <iam@ankurdubey.com>
2024-07-11 13:17:14 +0400

    fix: Disable doc tests

--- crates/routing-engine/src/blockchain/mod.rs
+++ crates/routing-engine/src/blockchain/mod.rs
@@ -2,8 +2,6 @@ use alloy::providers::RootProvider;
 use alloy::transports::http::Http;
 use reqwest::Client;
 
-use crate::blockchain::erc20::IERC20::IERC20Instance;
-
-pub type ERC20Contract = IERC20Instance<Http<Client>, RootProvider<Http<Client>>>;
+pub type ERC20Contract = erc20::IERC20::IERC20Instance<Http<Client>, RootProvider<Http<Client>>>;
 
 pub mod erc20;
